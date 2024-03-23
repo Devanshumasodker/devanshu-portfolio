9643ad9298e6b8c9ca81c982d8b250e83c2cf1c8
Devanshu Masodker <devanshumasodker357@gmail.com>
2024-03-23 21:10:38 +0530

    first commit

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Golam Morsed</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius officia ea ut vonsectetur adipisicing elit. Illo voluptatum iste, tempora rem obcaecati omnis delectus sunt, consequuntur, et ut quasi aperiam assumenda! <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex beatae reprehenderit vero maiores dignissimos error vitae, optio inventore explicabo quia officiis magni delectus? Lorem ipsum dolor, sit amet consectetur"
+        "<span>Hi, I am Devanshu</span> a passionate Web Designer and Developer I create modern designs and Websites.    "
       ],
       typeSpeed: 7,
       contentType: 'html'
